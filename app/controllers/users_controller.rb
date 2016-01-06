3d8599243157a4f74571ad0bfa3a44bc0b17b2f4
ouskah <ouskah.gn@gmail.com>
2016-01-06 13:48:44 +0000

    Ajout session de connexion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def home
+    
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+
   end
 
   def login
@@ -8,9 +13,12 @@ class UsersController < ApplicationController
   def check
     @current_user = User.where(name: params[:name], password: params[:password]).first
       if @current_user
+        session[:user_id] = @current_user.id
+        
         flash[:info] = "Bienvenue #{@current_user.name} !"
         redirect_to "/users/home"
       else
+        session[:user_id] = nil
         flash[:info] = "Échec de la connexion"
         redirect_to "/users/login"
       end
